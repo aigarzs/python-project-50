8ce21e9d458b168cdea5dd009a1aa4ffc00c0dea
Aigars Sinevics <aigarzs@gmail.com>
2023-12-21 22:42:00 +0200

    initial gendiff

---
+++ hexlet_code/scripts/gendiff.py
@@ -0,0 +1,8 @@
+import argparse
+
+def main():
+	parser = argparse.ArgumentParser(description="Compares two configuration files and shows a difference.")
+	parser.parse_args()
+
+if __name__=="__main__":
+        main()
