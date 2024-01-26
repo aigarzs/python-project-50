9b59d76e1f0db74f3e5c62b2f1b7d58f9773eda3
Aigars Sinevics <aigarzs@gmail.com>
2024-01-26 21:39:45 +0200

    Added first test

---
+++ tests/test_gendiff.py
@@ -0,0 +1,8 @@
+from gendiff import generate_diff
+
+
+def test_gendiff_1():
+    result = generate_diff("tests/fixtures/test1_file1.json",
+                           "tests/fixtures/test1_file2.json")
+    expected_result = open("tests/fixtures/test1_result").read()[:-1]
+    assert result == expected_result
